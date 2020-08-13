62904bc96bb0a9d815ed4f443f38e5f82ac28e91
HOURDOME <saengmueng_90@hotmail.com>
2020-08-13 19:46:35 +0700

    Part 6

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 export default function CharacterCard(props) {
-    const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false);
 
-    const activate = () => {
-        if(!active){
-            setActive(true)
-            props.activationHandler(props.value)
-        }
+  const attemptRef = useRef(props.attempt);
+
+  const activate = () => {
+    if (!active) {
+      setActive(true);
+      props.activationHandler(props.value);
     }
+  };
 
-    const className = `card ${active ? 'activeCard': ''}`
-    return (
-        <div className={className} onClick={activate}>{props.value}</div>
-    )
+  useEffect(() => {
+    if (attemptRef.current != props.attempt) {
+      setActive(false);
+      attemptRef.current = props.attempt;
+      console.log(attemptRef.current);
+    }
+  });
+
+  const className = `card ${active ? "activeCard" : ""}`;
+  return (
+    <div className={className} onClick={activate}>
+      {props.value}
+    </div>
+  );
 }
