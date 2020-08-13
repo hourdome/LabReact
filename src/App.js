0f9297bff633935c3e7dc62a0f599c8a54c82067
HOURDOME <saengmueng_90@hotmail.com>
2020-08-13 17:22:23 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
+const word =  "Hello";
 function App() {
   return (
     <div>
-      Hello World
+      {
+        <WordCard value={word}/>
+      }
     </div>
   );
 }
